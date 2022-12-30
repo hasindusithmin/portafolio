ecfa1c7230d75b3c775a8cd5bf718534c75c060c
hasindusithmin <hasindusithmin64@gmail.com>
2022-12-30 23:19:32 +0530

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
 
       <div className="w3-padding-large" id="main">
         <header className="w3-container w3-padding-32 w3-center w3-black" id="home">
-          <h1 className="w3-jumbo"><span className="w3-hide-small">I'm</span> John Doe.</h1>
-          <p>Photographer and Web Designer.</p>
-          <img src={profile} alt="boy" className="w3-image" width="992" height="1108" />
+          <h1 className="w3-jumbo"><span className="w3-hide-small">I'm</span> Hasindu Sithmin.</h1>
+          <p className='w3-xxlarge'>Developer.</p>
+          <img src={profile} alt="boy" className="w3-image w3-grayscale-max" width="992" height="1108" />
         </header>
       </div>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,31 +6,35 @@ export default function Navbar() {
     return (
         <>
             <nav className="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
-                <img src={profile} alt="profile" style={{width:'100%'}} />
-                    <a href="#" className="w3-bar-item w3-button w3-padding-large w3-black">
+                <img src={profile} alt="profile"  className="w3-opacity" style={{width:'100%'}} />
+                    <span href="#" className="w3-bar-item w3-button w3-padding-large w3-black">
                         <i className="fa fa-home w3-xxlarge"></i>
                         <p>HOME</p>
-                    </a>
-                    <a href="#about" className="w3-bar-item w3-button w3-padding-large w3-hover-black">
+                    </span>
+                    <span href="#" className="w3-bar-item w3-button w3-padding-large w3-hover-black">
                         <i className="fa fa-user w3-xxlarge"></i>
                         <p>ABOUT</p>
-                    </a>
-                    <a href="#photos" className="w3-bar-item w3-button w3-padding-large w3-hover-black">
-                        <i className="fa fa-eye w3-xxlarge"></i>
-                        <p>PHOTOS</p>
-                    </a>
-                    <a href="#contact" className="w3-bar-item w3-button w3-padding-large w3-hover-black">
-                        <i className="fa fa-envelope w3-xxlarge"></i>
-                        <p>CONTACT</p>
-                    </a>
+                    </span>
+                    <span href="#" className="w3-bar-item w3-button w3-padding-large w3-hover-black">
+                        <i className="fa fa-certificate w3-xxlarge"></i>
+                        <p>CERTIFICATES</p>
+                    </span>
+                    <span href="#" className="w3-bar-item w3-button w3-padding-large w3-hover-black">
+                        <i className="fa fa-hand-paper-o w3-xxlarge"></i>
+                        <p>SKILLS</p>
+                    </span>
+                    <span href="#" className="w3-bar-item w3-button w3-padding-large w3-hover-black">
+                        <i className="fa fa-product-hunt w3-xxlarge"></i>
+                        <p>PROJECT</p>
+                    </span>                  
             </nav>
 
             <div className="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
                 <div className="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
-                    <a href="#" className="w3-bar-item w3-button" style={{width:'20%'}}>HOME</a>
-                    <a href="#about" className="w3-bar-item w3-button" style={{width:'20%'}}>ABOUT</a>
-                    <a href="#photos" className="w3-bar-item w3-button" style={{width:'20%'}}>PHOTOS</a>
-                    <a href="#contact" className="w3-bar-item w3-button" style={{width:'20%'}}>CONTACT</a>
+                    <span href="#" className="w3-bar-item w3-button" style={{width:'20%'}}>HOME</span>
+                    <span href="#about" className="w3-bar-item w3-button" style={{width:'20%'}}>ABOUT</span>
+                    <span href="#photos" className="w3-bar-item w3-button" style={{width:'20%'}}>PHOTOS</span>
+                    <span href="#contact" className="w3-bar-item w3-button" style={{width:'20%'}}>CONTACT</span>
                 </div>
             </div>
 
